0727d8fccfe1f5a340d0aed78458d2f432c2049e
acano <abrahan.jcano99@gmail.com>
2025-03-01 11:16:28 -0500

    topbar generica de la aplicacion

---
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/composables/ThemeTopBar.kt
@@ -0,0 +1,19 @@
+package org.acano99.my_home.ui.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ThemeTopBar(title:String){
+    TopAppBar(title = { Text(title) }, actions = {
+        Icon(imageVector = Icons.Default.Settings, contentDescription = "", modifier = Modifier.clickable {  })
+    })
+}

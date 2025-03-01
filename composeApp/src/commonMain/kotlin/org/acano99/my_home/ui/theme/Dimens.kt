7c9854a8c23a9592076e8befce3c0893d8a036f6
acano <abrahan.jcano99@gmail.com>
2025-03-01 11:59:51 -0500

    valores actualizados

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/theme/Dimens.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/theme/Dimens.kt
@@ -2,7 +2,8 @@ package org.acano99.my_home.ui.theme
 
 import androidx.compose.ui.unit.dp
 
-val minPadding = 12.dp
+val minPadding = 8.dp
+val verySmallPadding = 12.dp
 val smallPadding = 16.dp
 val mediumPadding = 20.dp
 val highPadding = 24.dp

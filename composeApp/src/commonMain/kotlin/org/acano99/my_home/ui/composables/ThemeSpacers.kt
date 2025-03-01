1b0c9bbfa3f4c19724382df4d481d4744df543ba
acano <abrahan.jcano99@gmail.com>
2025-03-01 11:23:58 -0500

    creacion de los Spacers genericos

---
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/composables/ThemeSpacers.kt
@@ -0,0 +1,22 @@
+package org.acano99.my_home.ui.composables
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.acano99.my_home.ui.theme.highPadding
+import org.acano99.my_home.ui.theme.mediumPadding
+import org.acano99.my_home.ui.theme.minPadding
+import org.acano99.my_home.ui.theme.smallPadding
+
+@Composable
+fun MinSpacer() = Spacer(Modifier.height(minPadding))
+
+@Composable
+fun SmallSpacer() = Spacer(Modifier.height(smallPadding))
+
+@Composable
+fun MediumSpacer() = Spacer(Modifier.height(mediumPadding))
+
+@Composable
+fun HigSpacer() = Spacer(Modifier.height(highPadding))

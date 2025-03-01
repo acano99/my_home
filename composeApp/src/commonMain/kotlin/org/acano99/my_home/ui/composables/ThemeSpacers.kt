a8eee45cbdfe5488cb0364138c9430901d99286b
acano <abrahan.jcano99@gmail.com>
2025-03-01 12:00:03 -0500

    nuevo Spacer creado

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/composables/ThemeSpacers.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/composables/ThemeSpacers.kt
@@ -8,9 +8,13 @@ import org.acano99.my_home.ui.theme.highPadding
 import org.acano99.my_home.ui.theme.mediumPadding
 import org.acano99.my_home.ui.theme.minPadding
 import org.acano99.my_home.ui.theme.smallPadding
+import org.acano99.my_home.ui.theme.verySmallPadding
 
 @Composable
 fun MinSpacer() = Spacer(Modifier.height(minPadding))
+
+@Composable
+fun VerySmallSpacer() = Spacer(Modifier.height(verySmallPadding))
 
 @Composable
 fun SmallSpacer() = Spacer(Modifier.height(smallPadding))

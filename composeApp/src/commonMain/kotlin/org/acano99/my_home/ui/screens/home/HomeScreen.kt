c84d162fdc6c40409e722fcd21d3b6d49fd4e23b
acano <abrahan.jcano99@gmail.com>
2025-03-03 18:47:42 -0500

    actualizacion de la UI

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/home/HomeScreen.kt
@@ -6,19 +6,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ParagraphStyle
@@ -29,76 +34,144 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import myhome.composeapp.generated.resources.Res
+import myhome.composeapp.generated.resources.today
+import org.acano99.my_home.data.models.MenuType
 import org.acano99.my_home.data.models.buyModel
 import org.acano99.my_home.data.models.dayMenu
-import org.acano99.my_home.ui.composables.MinSpacer
-import org.acano99.my_home.ui.composables.SmallSpacer
+import org.acano99.my_home.ui.composables.HorizontalMinSpacer
+import org.acano99.my_home.ui.composables.HorizontalVerySmallSpacer
+import org.acano99.my_home.ui.composables.ThemeCard
+import org.acano99.my_home.ui.composables.ThemeFoodIcon
 import org.acano99.my_home.ui.composables.ThemeTopBar
-import org.acano99.my_home.ui.composables.VeryHigSpacer
-import org.acano99.my_home.ui.screens.common.CommonScreen
+import org.acano99.my_home.ui.composables.VerticalHigSpacer
+import org.acano99.my_home.ui.composables.VerticalMinSpacer
+import org.acano99.my_home.ui.composables.VerticalSmallSpacer
 import org.acano99.my_home.ui.theme.mediumPadding
 import org.acano99.my_home.ui.theme.smallPadding
 import org.acano99.my_home.ui.theme.verySmallPadding
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun HomeScreen() {
-    CommonScreen {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            topBar = { ThemeTopBar(title = "Home") },
-            floatingActionButton = {
-                FloatingActionButton(onClick = {}) {
-                    Icon(imageVector = Icons.Default.AddCircle, contentDescription = "")
-                }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { ThemeTopBar(title = "Home") },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.AddCircle, contentDescription = "")
             }
-        ) { innerPadding ->
-            Column(
-                modifier = Modifier.fillMaxSize().padding(innerPadding)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                Header(title = "12 de febrero")
-                VeryHigSpacer()
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+                .padding(horizontal = mediumPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            ThemeCard(modifier = Modifier.fillMaxWidth()) {
+                Header(
+                    modifier = Modifier.fillMaxWidth().padding(start = smallPadding),
+                    title = "12 de febrero"
+                )
+            }
+            VerticalHigSpacer()
+            ThemeCard(modifier = Modifier.fillMaxWidth()) {
                 Foods()
-                VeryHigSpacer()
-                BuyList()
-                VeryHigSpacer()
-                Pagos()
-                VeryHigSpacer()
             }
+            VerticalHigSpacer()
+            BuyList()
+            VerticalHigSpacer()
+            Pagos()
+            VerticalHigSpacer()
         }
     }
 }
 
 @Composable
-fun Header(title: String) {
-    Text(
-        title,
-        fontSize = 32.sp,
-        fontWeight = FontWeight.W700,
-        modifier = Modifier.padding(start = smallPadding)
-    )
+fun Header(modifier: Modifier = Modifier, title: String) {
+    val header = stringResource(Res.string.today)
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            modifier = Modifier.size(48.dp),
+            imageVector = Icons.Default.DateRange,
+            contentDescription = null
+        )
+        HorizontalVerySmallSpacer()
+        Text(text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(lineHeight = 34.sp)) {
+                withStyle(style = SpanStyle(fontWeight = FontWeight.W500)) {
+                    append(header)
+                }
+                append("\n")
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.W700,
+                        fontSize = 32.sp
+                    )
+                ) {
+                    append(title)
+                }
+            }
+
+        })
+    }
 }
 
 @Composable
 fun Foods() {
     Column(modifier = Modifier.padding(horizontal = smallPadding)) {
-        Text("Comidas planificadas", fontSize = 24.sp, fontWeight = FontWeight.W500)
-        MinSpacer()
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                modifier = Modifier.size(28.dp).border(
+                    border = BorderStroke(width = 1.dp, Color.Black),
+                    shape = RoundedCornerShape(24.dp)
+                )
+            )
+            HorizontalMinSpacer()
+            Text("Comidas planificadas", fontSize = 20.sp, fontWeight = FontWeight.W500)
+        }
+        VerticalMinSpacer()
         if (dayMenu.isNotEmpty()) {
             dayMenu.map { menu ->
-                Text(text = buildAnnotatedString {
-                    withStyle(style = ParagraphStyle(lineHeight = 24.sp)) {
-                        withStyle(
-                            style = SpanStyle(
-                                fontWeight = FontWeight.W500,
-                                fontSize = 18.sp
-                            )
-                        ) {
-                            append("${menu.type}: ")
-                        }
-                        append(menu.food)
+                Row(
+                    modifier = Modifier.padding(bottom = verySmallPadding),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    when (menu.type) {
+                        MenuType.Desayuno -> ThemeFoodIcon(
+                            icon = Icons.Default.ShoppingCart,
+                            backgroundColor = Color.Yellow
+                        )
+
+                        MenuType.Merienda -> ThemeFoodIcon(
+                            icon = Icons.Default.ShoppingCart,
+                            backgroundColor = Color.Cyan
+                        )
+
+                        MenuType.Almuerzo -> ThemeFoodIcon(
+                            icon = Icons.Default.ShoppingCart,
+                            backgroundColor = Color.Red
+                        )
+
+                        MenuType.Comida -> ThemeFoodIcon(icon = Icons.Default.ShoppingCart)
+                        null -> ThemeFoodIcon(icon = Icons.Default.ShoppingCart)
                     }
-                })
+                    HorizontalVerySmallSpacer()
+                    Text(text = buildAnnotatedString {
+                        withStyle(style = ParagraphStyle(lineHeight = 24.sp)) {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.W500,
+                                    fontSize = 18.sp
+                                )
+                            ) {
+                                append("${menu.type}: ")
+                            }
+                            append("\n")
+                            append(menu.food)
+                        }
+                    })
+                }
             }
         } else {
             Text("No hay comidas planificadas")
@@ -111,7 +184,7 @@ fun Foods() {
 fun BuyList() {
     Column(modifier = Modifier.padding(horizontal = smallPadding)) {
         Text("Compras planificadas", fontSize = 24.sp, fontWeight = FontWeight.W500)
-        MinSpacer()
+        VerticalMinSpacer()
 
         if (buyModel.items.isNotEmpty()) {
             FlowRow(modifier = Modifier.fillMaxWidth()) {
@@ -136,7 +209,7 @@ fun BuyList() {
 fun Pagos() {
     Column(modifier = Modifier.padding(horizontal = smallPadding)) {
         Text("Pagos planificados", fontSize = 24.sp, fontWeight = FontWeight.W500)
-        SmallSpacer()
+        VerticalSmallSpacer()
         (1..3).map {
             Box(
                 modifier = Modifier.fillMaxWidth().border(
@@ -146,11 +219,11 @@ fun Pagos() {
             ) {
                 Column(modifier = Modifier.padding(verySmallPadding)) {
                     Text("Factura XXXXX", fontSize = 18.sp, fontWeight = FontWeight.W500)
-                    MinSpacer()
+                    VerticalMinSpacer()
                     Text("150.00 Cup")
                 }
             }
-            SmallSpacer()
+            VerticalSmallSpacer()
         }
     }
 }

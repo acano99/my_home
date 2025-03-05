5c16a990eb9b18c697917ea85b0dfd680bee6fd6
acano <abrahan.jcano99@gmail.com>
2025-03-04 21:51:36 -0500

    creado la clase FoodHomeUiState

---
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/food/foodHome/FoodHomeUiState.kt
@@ -0,0 +1,9 @@
+package org.acano99.my_home.ui.screens.food.foodHome
+
+import org.acano99.my_home.data.models.DayMenuModel
+
+data class FoodHomeUiState(
+    val loading: Boolean = true,
+    val error: String = "",
+    val dayMenu: List<DayMenuModel> = listOf()
+)

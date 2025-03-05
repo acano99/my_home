467f58d08a188db27b5edb443db8d1d21e7db9a0
acano <abrahan.jcano99@gmail.com>
2025-03-04 23:33:35 -0500

    actualizacion de la navegacion

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/navigation/NavigationScreen.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/navigation/NavigationScreen.kt
@@ -4,22 +4,18 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import org.acano99.my_home.ui.screens.day.DayScreen
 import org.acano99.my_home.ui.screens.food.addFood.AddFoodHome
 import org.acano99.my_home.ui.screens.food.foodHome.FoodHomeScreen
 
 @Composable
 fun NavigationScreen() {
     val navHostController = rememberNavController()
-    NavHost(navController = navHostController, startDestination = "foodHome") {
+    NavHost(navController = navHostController, startDestination = "addFood") {
         composable(route = "foodHome") {
             FoodHomeScreen(navController = navHostController)
         }
         composable(route = "addFood") {
             AddFoodHome()
-        }
-        composable(route = "day") {
-            DayScreen()
         }
     }
 }

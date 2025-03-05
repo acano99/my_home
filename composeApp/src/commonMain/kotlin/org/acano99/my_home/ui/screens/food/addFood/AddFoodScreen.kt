096f2a3a98d77ab867b1b82a832ee7c604eb78d4
acano <abrahan.jcano99@gmail.com>
2025-03-04 21:06:39 -0500

    Actualizacion de la UI

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/food/addFood/AddFoodScreen.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/food/addFood/AddFoodScreen.kt
@@ -4,18 +4,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,21 +35,17 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
-import myhome.composeapp.generated.resources.Res
-import myhome.composeapp.generated.resources.comidasPlanificadas
 import org.acano99.my_home.data.models.MenuType
 import org.acano99.my_home.data.models.dayMenu
 import org.acano99.my_home.ui.composables.HorizontalVerySmallSpacer
 import org.acano99.my_home.ui.composables.ThemeCard
 import org.acano99.my_home.ui.composables.ThemeElevatedButton
 import org.acano99.my_home.ui.composables.ThemeFoodIcon
-import org.acano99.my_home.ui.composables.ThemeIconHeader
 import org.acano99.my_home.ui.composables.ThemeTopBar
 import org.acano99.my_home.ui.composables.VerticalHigSpacer
-import org.acano99.my_home.ui.composables.VerticalMinSpacer
 import org.acano99.my_home.ui.theme.mediumPadding
+import org.acano99.my_home.ui.theme.veryHighPadding
 import org.acano99.my_home.ui.theme.verySmallPadding
-import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
@@ -85,6 +86,7 @@ fun AddFoodHome() {
                     value = "", onValueChange = {}, label = { Text("Comida") })
                 ThemeElevatedButton()
             }
+            VerticalHigSpacer()
             dayMenu.map { menu ->
                 Row(
                     modifier = Modifier.padding(bottom = verySmallPadding),
@@ -124,64 +126,22 @@ fun AddFoodHome() {
                             append(menu.food)
                         }
                     })
+                    Spacer(Modifier.weight(1f))
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Rounded.Delete,
+                            contentDescription = "",
+                        )
+                    }
                 }
             }
+            ThemeElevatedButton()
+            Spacer(Modifier.height(veryHighPadding * 3))
         }
     }
 }
 
 @Composable
 fun Foods() {
-    ThemeCard {
-        ThemeIconHeader(
-            imageVector = Icons.Default.ShoppingCart,
-            title = stringResource(Res.string.comidasPlanificadas)
-        )
-        VerticalMinSpacer()
-        if (dayMenu.isNotEmpty()) {
-            dayMenu.map { menu ->
-                Row(
-                    modifier = Modifier.padding(bottom = verySmallPadding),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    when (menu.type) {
-                        MenuType.Desayuno -> ThemeFoodIcon(
-                            icon = Icons.Default.ShoppingCart,
-                            backgroundColor = Color.Yellow
-                        )
 
-                        MenuType.Merienda -> ThemeFoodIcon(
-                            icon = Icons.Default.ShoppingCart,
-                            backgroundColor = Color.Cyan
-                        )
-
-                        MenuType.Almuerzo -> ThemeFoodIcon(
-                            icon = Icons.Default.ShoppingCart,
-                            backgroundColor = Color.Red
-                        )
-
-                        MenuType.Comida -> ThemeFoodIcon(icon = Icons.Default.ShoppingCart)
-                        null -> ThemeFoodIcon(icon = Icons.Default.ShoppingCart)
-                    }
-                    HorizontalVerySmallSpacer()
-                    Text(text = buildAnnotatedString {
-                        withStyle(style = ParagraphStyle(lineHeight = 24.sp)) {
-                            withStyle(
-                                style = SpanStyle(
-                                    fontWeight = FontWeight.W500,
-                                    fontSize = 18.sp
-                                )
-                            ) {
-                                append("${menu.type}")
-                            }
-                            append("\n")
-                            append(menu.food)
-                        }
-                    })
-                }
-            }
-        } else {
-            Text("No hay comidas planificadas")
-        }
-    }
 }

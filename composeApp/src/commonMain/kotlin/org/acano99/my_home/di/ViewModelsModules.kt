b2b2200ac273e046b9950c57ea4b133923669507
acano <abrahan.jcano99@gmail.com>
2025-03-04 19:39:47 -0500

    Actualizacion del modulo de los viewmodels

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/di/ViewModelsModules.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/di/ViewModelsModules.kt
@@ -1,6 +1,6 @@
 package org.acano99.my_home.di
 
-import org.acano99.my_home.ui.screens.home.HomeViewModel
+import org.acano99.my_home.ui.screens.food.foodHome.HomeViewModel
 import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 

9264ee768af7b3a578f05a8992afb62588fb2370
acano <abrahan.jcano99@gmail.com>
2025-03-07 10:46:15 -0500

    actualizacion del componente

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/composables/ThemeElevatedButton.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/composables/ThemeElevatedButton.kt
@@ -3,6 +3,8 @@ package org.acano99.my_home.ui.composables
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,14 +14,17 @@ import org.acano99.my_home.ui.theme.minPadding
 import org.acano99.my_home.ui.theme.verySmallPadding
 
 @Composable
-fun ThemeElevatedButton() {
+fun ThemeElevatedButton(
+    modifier: Modifier = Modifier.fillMaxWidth().padding(
+        minPadding
+    ), colors: ButtonColors = ButtonDefaults.elevatedButtonColors(), text: String
+) {
     ElevatedButton(
         onClick = {},
         shape = RoundedCornerShape(verySmallPadding),
-        modifier = Modifier.fillMaxWidth().padding(
-            minPadding
-        )
+        modifier = modifier,
+        colors = colors
     ) {
-        Text("Planificar nueva comida", fontSize = 18.sp)
+        Text(text, fontSize = 18.sp)
     }
 }

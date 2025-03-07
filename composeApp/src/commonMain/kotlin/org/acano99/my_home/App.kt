7a557ff888d223dec8870a9b55f9421aa27067f2
acano <abrahan.jcano99@gmail.com>
2025-03-07 09:39:48 -0500

    actualizacion del App

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/App.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/App.kt
@@ -4,11 +4,14 @@ import androidx.compose.runtime.Composable
 import org.acano99.my_home.ui.screens.navigation.NavigationScreen
 import org.acano99.my_home.ui.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinContext
 
 @Composable
 @Preview
 fun App() {
-    AppTheme(darkTheme = true) {
-        NavigationScreen()
+    KoinContext {
+        AppTheme(darkTheme = true) {
+            NavigationScreen()
+        }
     }
 }

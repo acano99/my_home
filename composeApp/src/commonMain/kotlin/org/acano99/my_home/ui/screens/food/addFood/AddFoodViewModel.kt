1783f0a7c6f6fe6e8db0e8c1d4cf34c2ba2e6a6d
acano <abrahan.jcano99@gmail.com>
2025-03-07 09:57:07 -0500

    actualizacion del viewmodel AddFoodViewModel

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/food/addFood/AddFoodViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/food/addFood/AddFoodViewModel.kt
@@ -1,6 +1,15 @@
 package org.acano99.my_home.ui.screens.food.addFood
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class AddFoodViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(AddFoodUiState())
+    var uiState: StateFlow<AddFoodUiState> = _uiState
+
+    fun onMenuChange(menu: String) {
+        _uiState.update { state -> state.copy(menu = menu) }
+    }
 }

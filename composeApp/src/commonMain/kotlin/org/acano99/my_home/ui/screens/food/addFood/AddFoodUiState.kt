5f94d9a52d71b58dc9e21ca2f97281362280d7b3
acano <abrahan.jcano99@gmail.com>
2025-03-07 10:45:37 -0500

    actualizacion del estado de la ui

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/food/addFood/AddFoodUiState.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/food/addFood/AddFoodUiState.kt
@@ -1,5 +1,8 @@
 package org.acano99.my_home.ui.screens.food.addFood
 
+import org.acano99.my_home.data.models.DayMenuModel
+
 data class AddFoodUiState(
-    val menu: String = ""
+    val menu: String = "",
+    val dayMenu: List<DayMenuModel> = listOf()
 )

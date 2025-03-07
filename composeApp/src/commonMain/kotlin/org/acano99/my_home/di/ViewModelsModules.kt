5feda6661750e862a5cbc22c19fbb271d183cf46
acano <abrahan.jcano99@gmail.com>
2025-03-07 09:48:23 -0500

    agregado nuevo viewmodel AddFoodViewModel

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/di/ViewModelsModules.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/di/ViewModelsModules.kt
@@ -1,9 +1,11 @@
 package org.acano99.my_home.di
 
+import org.acano99.my_home.ui.screens.food.addFood.AddFoodViewModel
 import org.acano99.my_home.ui.screens.food.foodHome.FoodHomeViewModel
 import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::FoodHomeViewModel)
+    viewModelOf(::AddFoodViewModel)
 }

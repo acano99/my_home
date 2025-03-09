5583808b82c22ebc14e6e392367344a39c4f62e3
acano <abrahan.jcano99@gmail.com>
2025-03-09 03:53:31 -0400

    actualizacion de la inyeccion de dependencias

--- composeApp/src/androidMain/kotlin/org/acano99/my_home/di/KoinSetupModule.android.kt
+++ composeApp/src/androidMain/kotlin/org/acano99/my_home/di/KoinSetupModule.android.kt
@@ -1,8 +1,9 @@
 package org.acano99.my_home.di
 
+import org.acano99.my_home.data.database.getDatabaseBuilder
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val nativeModule: Module = module {
-    //single { getDatabaseBuilder(get()) }
+    single { getDatabaseBuilder(get()) }
 }

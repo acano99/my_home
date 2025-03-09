088e7f6a9aedce79165ad31d0c0031c494c62d74
acano <abrahan.jcano99@gmail.com>
2025-03-09 04:21:17 -0400

    creada la funcion de extension para convertir del dominio a la data

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/data/database/entity/DayMenuEntity.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/data/database/entity/DayMenuEntity.kt
@@ -2,10 +2,17 @@ package org.acano99.my_home.data.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.acano99.my_home.domain.model.DayMenu
 
 @Entity(tableName = "DayMenu")
 data class DayMenuEntity(
-    @PrimaryKey(autoGenerate = true) val id: Boolean,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val foods: List<FoodEntity>,
     val date: String
+)
+
+fun DayMenu.toData() = DayMenuEntity(
+    id = id,
+    foods = foods.map { it.toData() },
+    date = date
 )

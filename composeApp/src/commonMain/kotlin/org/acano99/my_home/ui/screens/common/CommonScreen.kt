0223705065906f9c368f075a8b2156c46053cf12
acano <abrahan.jcano99@gmail.com>
2025-03-09 03:51:24 -0400

    creacion de la plantilla para las screen

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/common/CommonScreen.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/common/CommonScreen.kt
@@ -1,13 +1,31 @@
 package org.acano99.my_home.ui.screens.common
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.acano99.my_home.ui.composables.ThemeBottomNavigationBar
+import org.acano99.my_home.ui.composables.ThemeTopBar
+import org.acano99.my_home.ui.theme.mediumPadding
 
 @Composable
-fun CommonScreen(content: @Composable () -> Unit) {
-    Surface(
+fun CommonScreen(content: @Composable ColumnScope.() -> Unit) {
+    Scaffold(
         modifier = Modifier.fillMaxSize(),
-        content = content)
+        topBar = { ThemeTopBar(title = "Comidas") },
+        floatingActionButton = {},
+        bottomBar = { ThemeBottomNavigationBar() }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+                .padding(mediumPadding)
+                .verticalScroll(rememberScrollState()), content = content
+        )
+    }
 }
+

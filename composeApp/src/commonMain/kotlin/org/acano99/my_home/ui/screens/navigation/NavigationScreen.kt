5e66786d58d4df8cc3eeee5bcc402279345ec8b2
acano <abrahan.jcano99@gmail.com>
2025-03-09 03:54:17 -0400

    actualizacion de la navegacion

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/navigation/NavigationScreen.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/ui/screens/navigation/NavigationScreen.kt
@@ -4,9 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import org.acano99.my_home.ui.screens.buyList.BuildListHomeScreen
 import org.acano99.my_home.ui.screens.food.addFood.AddFoodHome
 import org.acano99.my_home.ui.screens.food.foodHome.FoodHomeScreen
+import org.koin.core.annotation.KoinExperimentalAPI
 
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 fun NavigationScreen() {
     val navHostController = rememberNavController()
@@ -16,6 +19,9 @@ fun NavigationScreen() {
         }
         composable(route = "addFood") {
             AddFoodHome()
+        }
+        composable(route = "buildList") {
+            BuildListHomeScreen(navController = navHostController)
         }
     }
 }

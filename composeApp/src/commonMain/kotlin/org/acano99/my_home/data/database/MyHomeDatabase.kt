04ee31cb5d012fb00393cb1f114468a025f5180f
acano <abrahan.jcano99@gmail.com>
2025-03-09 03:53:55 -0400

    creacion de la base de datos

---
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/data/database/MyHomeDatabase.kt
@@ -0,0 +1,22 @@
+package org.acano99.my_home.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import org.acano99.my_home.data.database.dao.FoodDao
+import org.acano99.my_home.data.database.entity.FoodEntity
+import org.acano99.my_home.data.models.DayMenuModel
+
+const val DATABASE_NAME = "myHome.db"
+
+interface FixRoomError {
+    fun clearAllTables()
+}
+
+@Database(entities = [DayMenuModel::class, FoodEntity::class], version = 1)
+abstract class MyHomeDatabase : RoomDatabase(), FixRoomError {
+    abstract fun myHomeDao(): MyHomeDao
+
+    abstract fun foodDao(): FoodDao
+
+    override fun clearAllTables() {}
+}

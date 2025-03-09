0ffc5c198462c10c3f991c0e12bf5f7f5b719fee
acano <abrahan.jcano99@gmail.com>
2025-03-09 03:53:35 -0400

    actualizacion de la inyeccion de dependencias

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/di/KoinSetupModule.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/di/KoinSetupModule.kt
@@ -4,11 +4,11 @@ import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
 
-expect val nativeModule:Module
+expect val nativeModule: Module
 
-fun initKoin(config:KoinAppDeclaration? = null){
-    startKoin{
+fun initKoin(config: KoinAppDeclaration? = null) {
+    startKoin {
         config?.invoke(this)
-        modules(nativeModule, viewModelModule)
+        modules(nativeModule, viewModelModule, databaseModule)
     }
 }

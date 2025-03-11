563b7ea39b061302403a61a875a1d70de922084a
acano <abrahan.jcano99@gmail.com>
2025-03-11 02:34:09 -0400

    agregado el DayMenuRepository a la inyeccion de dependencias

--- composeApp/src/commonMain/kotlin/org/acano99/my_home/di/DatabaseModule.kt
+++ composeApp/src/commonMain/kotlin/org/acano99/my_home/di/DatabaseModule.kt
@@ -3,6 +3,10 @@ package org.acano99.my_home.di
 import androidx.room.RoomDatabase
 import org.acano99.my_home.data.database.MyHomeDatabase
 import org.acano99.my_home.data.database.dao.DayMenuDao
+import org.acano99.my_home.data.database.repositoryImplement.DayMenuRepository
+import org.acano99.my_home.domain.repository.IDayMenuRepository
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val databaseModule = module {
@@ -10,4 +14,5 @@ val databaseModule = module {
         val dbBuilder = get<RoomDatabase.Builder<MyHomeDatabase>>()
         dbBuilder.build().dayMenuDao()
     }
+    factoryOf(::DayMenuRepository).bind<IDayMenuRepository>()
 }
